12db7579a3530aa99a89b44de53575a54f707322
RicardoD92 <duchiarg@gmail.com>
2022-08-27 19:26:17 -0300

    ajustes

---
+++ proyectoIntegrador/ProyectoIntegrador_DuchiRicardo/BackEnd/src/main/java/com/Portfolio/RAD/RadApplication.java
@@ -0,0 +1,13 @@
+package com.Portfolio.RAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadApplication.class, args);
+	}
+
+}
---
+++ proyectoIntegrador/ProyectoIntegrador_DuchiRicardo/BackEnd/src/test/java/com/Portfolio/RAD/RadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.RAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
