2906af30bf4c0b07af64c0c809159a98ffd4694e
RicardoD92 <duchiarg@gmail.com>
2022-08-31 09:39:21 -0300

    JWT

--- proyectoIntegrador/ProyectoIntegrador_DuchiRicardo/BackEnd/src/main/java/com/Portfolio/RAD/Controller/PersonaController.java
+++ proyectoIntegrador/ProyectoIntegrador_DuchiRicardo/BackEnd/src/main/java/com/Portfolio/RAD/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.Portfolio.RAD.Entity.Persona;
 import com.Portfolio.RAD.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
--- proyectoIntegrador/ProyectoIntegrador_DuchiRicardo/FrontEnd/src/app/app.module.ts
+++ proyectoIntegrador/ProyectoIntegrador_DuchiRicardo/FrontEnd/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {HttpClientModule }  from  '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -29,7 +31,9 @@ import {HttpClientModule }  from  '@angular/common/http';
     EducacionComponent,
     SkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
